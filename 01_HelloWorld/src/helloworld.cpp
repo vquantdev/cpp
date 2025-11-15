1132afe073a3d2da1796ffae6df5810be3dc9669
Veniamin <vquantdev@gmail.com>
2025-11-15 19:16:38 +0000

    Hello World

---
+++ 01_HelloWorld/src/helloworld.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+int main() {
+
+    std::cout << "I like Pizza!" << std::endl;
+    std::cout << "I also like Coca-Cola!" << std::endl;
+
+    std::cout << "I like Gaming!" << "\n";
+    std::cout << "I like Reading!" << "\n";
+
+    return 0;
+}
